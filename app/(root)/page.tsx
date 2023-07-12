5af367d3e86649a40b1a98fbd71f860205efab87
Denio Nhanale <dnhanale@gmail.com>
2023-07-12 17:29:07 +0200

    Adding Authentication

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+const SetupPage = () => {
     return (
      <div className="p-4">
-        <Button>Hello</Button>
+       <UserButton afterSignOutUrl="/" />
      </div>
-    )
+    );
   }
+
+  export default SetupPage;
   
